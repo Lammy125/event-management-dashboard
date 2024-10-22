e71808ae21adb477918dc3d13aa92341b4b83350
Adeyemo Peter <adeyemoolamide47@gmail.com>
2024-10-22 10:12:34 +0100

    added sidebar for mobile view, images and styles

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,11 @@
+// Function for DarkMode
 document
   .getElementById("dark-mode-toggle")
   .addEventListener("change", function () {
     document.body.classList.toggle("dark-mode");
   });
 
+// Function for collapsible sidebar
 document.addEventListener("DOMContentLoaded", function () {
   const collapseButton = document.getElementById("collapse");
   const sidebar = document.querySelector(".sideBar");
@@ -12,4 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
   collapseButton.addEventListener("click", function () {
     sidebar.classList.toggle("collapsed");
   });
+});
+
+// Select the elements for Mobile sidebar
+const sidebar = document.getElementById("sidebarMobile");
+const menuIcon = document.querySelector(".menuIcon");
+
+// Toggle sidebar visibility on hamburger menu click
+menuIcon.addEventListener("click", function () {
+  sidebar.classList.toggle("active");
+  menuIcon.classList.toggle("active");
 });
